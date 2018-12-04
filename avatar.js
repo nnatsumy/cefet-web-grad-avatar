f631bbc0aa7aa84f46d5cc3993d17a6c00c7c1ad
Aluno <augustocosta2609@gmail.com>
2018-12-04 11:20:34 -0200

    Exercicio 2 - Add skin color to avatar

--- avatar.js
+++ avatar.js
@@ -1,7 +1,16 @@
 let name = document.querySelector('#name'); 
 let avatarnome = document.querySelector('#avatar-nome'); 
+let cor = document.querySelector('#cor-pele'); 
+
+let corcorpo = document.querySelector('#avatar-corpo'); 
+let corcabeca = document.querySelector('#avatar-cabeca'); 
 
 name.addEventListener('change', function(e) {
   document.getElementById("avatar-nome").innerHTML = name.value;
 });
 
+
+cor.addEventListener('change', function(e) {
+  corcorpo.style.backgroundColor = cor.value;
+  corcabeca.style.backgroundColor = cor.value;
+});
