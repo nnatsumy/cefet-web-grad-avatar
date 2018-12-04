3781dcbe71d1755cbb606851ffc4427dc61e08c0
Aluno <augustocosta2609@gmail.com>
2018-12-04 11:38:40 -0200

    Exercicio 3- Add hair style

--- avatar.js
+++ avatar.js
@@ -1,9 +1,11 @@
 let name = document.querySelector('#name'); 
 let avatarnome = document.querySelector('#avatar-nome'); 
 let cor = document.querySelector('#cor-pele'); 
+let cabelo = document.querySelector('#cabelo'); 
 
 let corcorpo = document.querySelector('#avatar-corpo'); 
 let corcabeca = document.querySelector('#avatar-cabeca'); 
+let corcabelo = document.querySelector('#avatar-cabelo'); 
 
 name.addEventListener('change', function(e) {
   document.getElementById("avatar-nome").innerHTML = name.value;
@@ -13,4 +15,8 @@ name.addEventListener('change', function(e) {
 cor.addEventListener('change', function(e) {
   corcorpo.style.backgroundColor = cor.value;
   corcabeca.style.backgroundColor = cor.value;
+});
+
+cabelo.addEventListener('change', function(e) {
+  corcabelo.src = cabelo.value;
 });
